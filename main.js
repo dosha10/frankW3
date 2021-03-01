0de3e0a6642afa18282f93f80dd04f55a0d71e6e
Francis <ndungufrank01@gmail.com>
2021-03-01 12:35:29 +0300

    commit

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#hide-design").click(function() {
+        $("#hide-design").hide();
+        $("#show-design").show();
+    });
+    $("#show-design").click(function() {
+        $("#hide-design").show();
+        $("#show-design").hide();
+    });
+    $("#hide-development").click(function() {
+        $("#hide-development").hide();
+        $("#show-development").show();
+    });
+    $("#show-development").click(function() {
+        $("#hide-development").show();
+        $("#show-development").hide();
+    });
+    $("#hide-pm").click(function() {
+        $("#hide-pm").hide();
+        $("#show-pm").show();
+    });
+    $("#show-pm").click(function() {
+        $("#hide-pm").show();
+        $("#show-pm").hide();
+    });
+
+
+
+
+
+
+
+
+
+
+});
